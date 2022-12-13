540ab6566fdad4df9348baee60d5c8e113b5ca93
Jason Garner <elevatorguy@gmail.com>
2022-12-12 19:09:54 -0600

    what it be - seemingly

--- src/gravity_force_generator.cpp
+++ src/gravity_force_generator.cpp
@@ -1,18 +1,5 @@
 #include "../include/gravity_force_generator.h"
 
-//may need improvement - reference: https://nssdc.gsfc.nasa.gov/planetary/factsheet/planet_table_ratio.html
-#define SUN_GRAVITY (274)
-#define EARTH_GRAVITY (9.81)
-#define MERCURY_GRAVITY (0.378*EARTH_GRAVITY)
-#define VENUS_GRAVITY (0.907*EARTH_GRAVITY)
-#define MOON_GRAVITY (0.166*EARTH_GRAVITY)
-#define MARS_GRAVITY (0.377*EARTH_GRAVITY)
-#define JUPITER_GRAVITY (2.36*EARTH_GRAVITY)
-#define SATURN_GRAVITY (0.916*EARTH_GRAVITY)
-#define URANUS_GRAVITY (0.889*EARTH_GRAVITY)
-#define NEPTUNE_GRAVITY (1.12*EARTH_GRAVITY)
-#define PLUTO_GRAVITY (0.071*EARTH_GRAVITY)
-
 atg_scs::GravityForceGenerator::GravityForceGenerator() {
     m_g = EARTH_GRAVITY;
 }
